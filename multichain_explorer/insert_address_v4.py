9d1b55eb9fe4f942811163477e8ef65886eae825
parishilan_auxesis <parishilan@auxesisgroup.com>
2018-05-29 14:26:42 +0530

    v5 crawler

---
+++ multichain_explorer/insert_address_v4.py
@@ -0,0 +1,130 @@
+__author__ = 'udasi'
+import config
+from wrap_core import *
+import time
+import logging
+import datetime
+import sys
+
+db_conn = config.mongo_conn().rajchain_explorer
+redis_conn = config.redis_conn()
+
+str_date = datetime.datetime.now().strftime('%Y%m%d')
+logger = config.logger
+_file = logger['mempool']['path'] + 'multichain_addressv4_%s' % str_date
+logging.basicConfig(filename=_file,
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
+_logging = logging.getLogger('explorer')
+_logging.addHandler(logging.StreamHandler(sys.stdout))
+
+
+class exec_time():
+    def __init__(self):
+        self.start_epoch = time.time()
+
+    def get_duration(self):
+        self.end_epoch = time.time()
+        duration = self.end_epoch - self.start_epoch
+        return duration
+
+
+# current_block_height = getblockcount(config.testnet['btc_prefix'], config.payload)
+current_block_height = custom_rpc('getblockcount')
+# print current_block_height
+logging.info('crawling blocks upto height:::: %s' % current_block_height)
+set_current_block_height_in_redis = redis_conn.set('multichain_current_block_height', current_block_height)
+if set_current_block_height_in_redis is True:
+    logging.info('setting current blockheight in redis::: %s' % set_current_block_height_in_redis)
+else:
+    logging.info('setting current blockheight in redis failed' % set_current_block_height_in_redis)
+
+crawled_block_height = int(redis_conn.get('masschain_crawled_block_height'))
+logging.info('blocks crawled upto height::: %s' % crawled_block_height)
+
+
+def get_txs(block_height):
+    # block_hash = getblockhash(config.testnet['btc_prefix'], config.payload, block_height)
+    # block_details = getblock(config.testnet['btc_prefix'], config.payload, block_hash)
+    block_hash = custom_rpc('getblockhash',[int(block_height)])
+    block_details = custom_rpc('getblock',[block_hash])
+    transactions_list = block_details['tx']
+    logging.info('crawling block height:: %s, with transactions length:: %s, and transactions:: %s' % (
+        block_height, len(transactions_list), transactions_list))
+    return transactions_list
+
+
+def address_crawler_1():
+    for block_height in xrange(crawled_block_height, current_block_height + 1):
+        block_start_time = exec_time()
+        redis_conn.set('masschain_crawled_block_height', block_height)
+        logging.debug('crawling block :: %s', block_height)
+        transaction_list = get_txs(str(block_height))
+        for transaction in transaction_list:
+            logging.info('crawling transaction:: %s' % transaction)
+
+            start_time = exec_time()
+            # transaction_details = getrawtransaction(config.testnet['btc_prefix'], config.payload, transaction)
+            transaction_details = custom_rpc('getrawtransaction',[transaction,1])
+            epoch = transaction_details['time']
+            vout_details = transaction_details['vout']
+            vin_details = transaction_details['vin']
+            for vout in vout_details:
+                if 'scriptPubKey' in vout:
+                    if 'addresses' in vout['scriptPubKey']:
+                        address = vout['scriptPubKey']['addresses'][0]
+                        balance = vout['value']
+                        index = vout['n']
+                        vout_update_dict = {
+                            'txid': transaction,
+                            'balance': balance,
+                            'index': index,
+                            'spent': False,
+                            'epoch': epoch
+                        }
+                        exists_flag = db_conn.address_details_v3.find_one({"txs":{"$elemMatch":{"eq":vout_update_dict}}})
+                        # if exists_flag:
+                        # print '1111111111111111111111111 %s' %exists_flag
+                        if not exists_flag:
+                            vout_update_result = db_conn.address_details_v3.update({'address': address},
+                                                                                   {"$push": {"txs": vout_update_dict}},
+                                                                                   True)
+                            logging.info('updated vout of address:: %s, inserted vout_dict:: %s, result is:: %s' % (
+                            address, vout_update_dict, vout_update_result))
+                        else:
+                            logging.info('not updating vout of address:: %s, not inserted vout_dict:: %s' % (
+                            address, vout_update_dict))
+            for vin in vin_details:
+                if 'coinbase' in vin:
+                    pass
+                if 'txid' in vin:
+                    vin_txid = vin['txid']
+                    vin_index = vin['vout']
+                    update_query = {
+                        "txs.txid": vin_txid,
+                        "txs.index": vin_index
+                    }
+                    update_params = {
+                        'txs.$.spent': True,
+                        'txs.$.spent_txid': transaction
+                    }
+                    vin_update_result = db_conn.address_details_v3.update(update_query, {"$set": update_params})
+                    logging.info('updated vin matching params:: %s, updating params:: %s, result is:: %s' % (
+                                update_query, update_params, vin_update_result))
+            end_time = start_time.get_duration()
+            logging.debug('time taken for transaction:: %s is:: %s' % (transaction, end_time))
+        block_end_time = block_start_time.get_duration()
+        print 'time taken to crawl block: %s, with transactions: %s, is:::: %s' \
+              % (block_height, len(transaction_list), block_end_time)
+        logging.debug('time taken to crawl block: %s, with transactions: %s, is:::: %s' \
+                      % (block_height, len(transaction_list), block_end_time))
+
+def crawl_address():
+    if redis_conn.exists('address_mutex') is not True:
+        redis_conn.set('address_mutex',True)
+        address_crawler_1()
+        redis_conn.delete('address_mutex')
+
+crawl_address()
---
+++ multichain_explorer/insert_address_v5.py
@@ -0,0 +1,179 @@
+__author__ = 'cryptoKTM'
+import requests,json,sys,os,pymongo
+import config
+from wrap_core import *
+import time
+import logging
+import datetime
+import sys
+
+
+import requests,json,sys,os,pymongo
+
+from pymongo import MongoClient
+
+
+db_conn = config.mongo_conn().rajchain_explorer
+redis_conn = config.redis_conn()
+
+
+conf = {
+    'rpc_port': 2332,
+    'user_name': 'masscoinrpc',
+    'password': '9Gjz8kSHqpEafq6DUaisePcnf6jSXiXcDKiUsfvwbMvC',
+    'root': '1MNbWhUdFN2K7cTsehupp38zE79W6RvxfWprTf'
+}
+
+
+url = 'http://' + conf['user_name'] + ':' + conf['password'] + '@165.227.116.240:' + str(conf['rpc_port']) + '/'
+
+# url="http://asd:asdf@localhost:2332"
+payload={}
+payload = {"jsonrpc":1,"id":"curltext"}
+
+ 
+# url="http://masscoinrpc:00AD1197627954913F825A16CFF4D4503E45616639C97543EA1C439ED9C735DC@localhost:2332"
+   
+# url="http://masscoinrpc:00AD1197627954913F825A16CFF4D4503E45616639C97543EA1C439ED9C735DC@165.227.116.61:2332"
+ 
+
+
+class wrapcoreException(Exception): 
+	def __init__(self, value): 
+		self.parameter = value 
+	def __str__(self): 
+		return repr(self.parameter)
+
+
+
+
+
+def get_block_count():
+	payload["method"] = "getblockcount"
+	payload["params"] =[]
+	# print payload
+	response = requests.post(url,json.dumps(payload))
+	if response.status_code != 200:
+		raise wrapcoreException ('')
+	return(response.json()["result"])
+
+def gettxid(block_no):
+	# print"into gettxid"
+	print "crawling block no block_no_ %s" %block_no	
+	payload["method"]="getblockhash"
+	payload["params"]=[block_no]
+	resp = requests.post(url,json.dumps(payload))
+	if resp.status_code != 200:
+		raise wrapcoreException (block_no)
+	else:
+		block_hash = resp.json()["result"]
+		payload["method"]="getblock"
+		payload["params"]=[block_hash]
+		resp = requests.post(url,json.dumps(payload))
+		if resp.status_code == 200:
+			return resp.json()["result"]["tx"]
+		else:
+			raise wrapcoreException("getblock error")
+
+def add_from_txid(txid,block_no_):
+	try:
+		vin_addresses = []     
+		vout_addresses=[]
+		payload["method"]="getrawtransaction"
+		payload["params"]=[txid,1]
+		response = requests.post(url,json.dumps(payload))
+		if response.status_code != 200:
+			# raise Exception ('rpc error')
+			raise wrapcoreException ("(line 95)vin_add ERROR for txid:: "+txid)
+		response =response.json()
+		# print "res is %s " %response
+		timestamp = response["result"]["time"]
+		block_no = block_no_
+		response_vout = response["result"]["vout"]
+		response_vin= response["result"]["vin"] 
+
+		for data in response_vout:
+			vout_json = {}
+			vout_json["address"]=data["scriptPubKey"]["addresses"][0]
+			vout_json["value"]=data["value"]
+			vout_json["index"]=data["n"]
+			vout_json["block_no"] =block_no
+			vout_json["timestamp"] = timestamp
+			vout_json["txid"]= txid
+			vout_addresses.append(vout_json)
+
+			exists_flag = db_conn.address_details_v5.find_one(vout_json)
+			if not exists_flag:
+				vout_insert_result = db_conn.address_details_v5.insert(vout_json)
+
+		for data in response_vin:
+			if data.__contains__('coinbase'):
+				pass
+				
+
+			else:
+				raw_tx1=data["txid"]
+				vout_int=data["vout"]  #vout index
+				payload["params"]=[raw_tx1,1]
+				response_ = requests.post(url,json.dumps(payload))
+				response_= response_.json()
+				timestamp_ = response_["result"]["time"]
+				data_ =response_["result"]["vout"]
+				for item in data_:
+					if item["n"] == vout_int:
+						vin_json ={}
+						vin_json["address"]=item["scriptPubKey"]["addresses"][0] 
+						# vin_json["spent_timestamp"]=item["timestamp"]
+						find_params={"address":vin_json["address"],"txid":raw_tx1}
+
+						# exists_flag = db_conn.address_details_v5.find_one(find_params)
+						# if exists_flag:
+						update_flag = db_conn.address_details_v5.update(find_params,{"$set":{"spent_timestamp":timestamp,"spent_txid":txid}})
+						# vin_json["spent_timestamp"]=item["timestamp"]
+						# balance=item["value"]
+					
+						vin_addresses.append(vin_json)
+
+
+
+		# print vout_addresses
+		if vout_addresses and vin_addresses:
+			return {"vout_add":vout_addresses,"vin_add":vin_addresses}
+
+
+	except Exception as e:
+
+		exc_type, exc_obj, exc_tb = sys.exc_info()
+		fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1] 
+		print(exc_type, fname, exc_tb.tb_lineno, e.message)
+
+
+try:
+	crawled_block = int(redis_conn.get('crawled_block'))
+	# crawled_block += 1  #is the no of block that is going to get crawled now
+	block_count = int(get_block_count())
+	final={}
+
+	while((crawled_block+1) < block_count):
+		# print 
+		tx_array=gettxid((crawled_block+1))
+		for data in tx_array:
+			txid = str(data)
+			a = add_from_txid(txid,(crawled_block+1))
+			# if not a:
+			# 	raise wrapcoreException('error on ')
+		# final["block_no"]=crawled_block
+		
+		redis_conn.set('crawled_block',(crawled_block+1))
+		crawled_block = int(redis_conn.get('crawled_block'))
+		block_count = int(get_block_count())
+		# print final
+		# block_count+=1
+		# crawled_block +=1
+except wrapcoreException as we:
+	print we.parameter
+except Exception as e:
+	exc_type, exc_obj, exc_tb = sys.exc_info()  
+	fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1] 
+	print "exception" 
+	print(exc_type, fname, exc_tb.tb_lineno, e.message)
